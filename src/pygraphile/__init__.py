b6a8645e9161e0221b3343658ed88666b382513c
Dipankar Shaw <dipankarshaw692@gmail.com>
2025-11-09 22:29:32 +0530

    feat(core): add core PyGraphile class and db schema discovery
    Introduce the main `PyGraphile` class for database connection and introspection.
    This sets up an SQLite connection, discovers tables, and retrieves their schemas
    using the new `get_schema_from_table_name` helper.

--- src/pygraphile/__init__.py
+++ src/pygraphile/__init__.py
@@ -1,3 +1,4 @@
+import sqlite3
 """
 PyGraphile - A Python library for automatically generating GraphQL APIs from databases.
 
@@ -9,5 +10,49 @@ Currently supports SQLite with plans for PostgreSQL, MySQL, and other databases.
 
 __version__ = "0.1.0"
 __author__ = "dshaw0004"
-__all__ = ["__version__"]
+__all__ = ["__version__", "PyGraphile"]
+
+
+def get_schema_from_table_name(cursor, tablename: str) -> list[dict]:
+    '''
+    PRAGMA table_info outputs cid, name, type, notnull, dflt_value, pk
+    example:
+    [(0, 'id', 'INTEGER', 0, None, 1), (1, 'name', 'TEXT', 0, None, 0)]
+    '''
+    result: list = cursor.execute(f"PRAGMA table_info({tablename})").fetchall()
+    schema = [{
+        'cid': res[0],
+        'name': res[1],
+        'type': res[2],
+        'notnull': res[3],
+        'default': res[4],
+        'primary_key': res[5],
+    } for res in result]
+    return schema
+
+
+class PyGraphile:
+    def __init__(self,
+                 db_name: str = 'pygraphile.sqlite',
+                 db_type: str = 'sqlite3',
+                 migration_folder: str = 'nomigration',
+                 ):
+
+        if 'sqlite3' != db_type:
+            print('this database is not supported yet')
+            exit(1)
+
+        if 'nomigration' != migration_folder:
+            # TODO: apply migration
+            print('need to implement this')
+
+        self.con = sqlite3.connect(db_name)
+        self.cursor = self.con.cursor()
+
+        result: list[tuple[str,]] = self.cursor.execute(
+            "SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%'").fetchall()
+        self.tables = [res[0] for res in result]
+        print(f'Following tables detected {self.tables}')
+
+        self.table_schemas = get_schema_from_table_name(self.cursor, self.tables[0])
 
